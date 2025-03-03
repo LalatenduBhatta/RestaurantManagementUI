fa954fa545965269a3539ef7dda95b61557c2e2e
Lalatendu Bhatta <lalatendubhatta@gmail.com>
2025-03-03 16:43:52 +0530

    login auth logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 function App() {
+  const navigate = useNavigate()
+  const token = localStorage.getItem("id")
+  useEffect(() => {
+    if (!token) {
+      navigate("/login")
+    }
+  })
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <div className="h-screen fixed z-50 overflow-y-scroll no-scrollbar w-[15%]">
-        <Sidebar />
-      </div>
+    <>
+      {
+        token && <div className="flex">
+          {/* Sidebar */}
+          <div className="h-screen fixed z-50 overflow-y-scroll no-scrollbar w-[15%]">
+            <Sidebar />
+          </div>
 
-      {/* main section */}
-      <div className="flex-1 flex flex-col ms-[15%]">
-        <div className="">
-          {/* navbar */}
-          <Navbar />
+          {/* main section */}
+          <div className="flex-1 flex flex-col ms-[15%]">
+            <div className="">
+              {/* navbar */}
+              <Navbar />
+            </div>
+            <div className="flex-1 bg-gray-50">
+              {/* content */}
+              <Outlet />
+            </div>
+          </div>
         </div>
-        <div className="flex-1 bg-gray-50">
-          {/* content */}
-          <Outlet />
-        </div>
-      </div>
-
-    </div>
+      }
+    </>
   )
 }
 export default App
